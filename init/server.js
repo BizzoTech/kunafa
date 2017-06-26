586b456df500fd8f0cc02a7099030df363d29422
Emad Shaaban <emad.shaaban92@gmail.com>
2017-06-26 17:54:04 +0200

    more robust init script

--- init/server.js
+++ init/server.js
@@ -6,109 +6,144 @@ const PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-find'));
 
 const {
-  mainDb,
-  eventsDb,
-  notificationsDb,
-  archiveDb,
-  sharedDb,
-  publicDb,
-  authDb,
-  anonymousDb
+ mainDb,
+ eventsDb,
+ notificationsDb,
+ archiveDb,
+ sharedDb,
+ publicDb,
+ authDb,
+ anonymousDb
 } = require('experimental-server');
 
-const COUCHDB_USER = process.env.COUCHDB_USER;
-const COUCHDB_PASSWORD = process.env.COUCHDB_PASSWORD;
+(async() => {
+ try {
 
-const mainReplicatorUrl = "http://main-db:5984/_replicator";
-const eventsReplicatorUrl = "http://events-db:5984/_replicator";
-const notificationsReplicatorUrl = "http://notifications-db:5984/_replicator";
-const archiveReplicatorUrl = "http://archive-db:5984/_replicator";
+  const COUCHDB_USER = process.env.COUCHDB_USER;
+  const COUCHDB_PASSWORD = process.env.COUCHDB_PASSWORD;
 
-const createDB = dbUrl => {
-  return new PouchDB(dbUrl, {
-  	ajax: {
-  		cache: false,
-  		timeout: 60000
-  	},
-  	auth: {
-  		username: COUCHDB_USER,
-  		password: COUCHDB_PASSWORD
-  	}
-  });
-}
+  const mainReplicatorUrl = "http://main-db:5984/_replicator";
+  const eventsReplicatorUrl = "http://events-db:5984/_replicator";
+  const notificationsReplicatorUrl = "http://notifications-db:5984/_replicator";
+  const archiveReplicatorUrl = "http://archive-db:5984/_replicator";
 
-const mainReplicator = createDB(mainReplicatorUrl);
-const eventsReplicator = createDB(eventsReplicatorUrl);
-const notificationsReplicator = createDB(notificationsReplicatorUrl);
-const archiveReplicator = createDB(archiveReplicatorUrl);
-
-
-const mainDbUrl = `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@main-db:5984/db`;
-const eventsDbUrl = `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@events-db:5984/events`;
-const notificationsDbUrl = `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@notifications-db:5984/notifications`;
-const archiveDbUrl = `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@archive-db:5984/archive`;
-
-
-fetch(`http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@auth-db:5984/_users`, { method: 'PUT'});
-
-for(db of [mainDb, eventsDb, notificationsDb, archiveDb, sharedDb, publicDb, anonymousDb]){
-  db.createIndex({
-    index: {
-      fields: [{
-        createdAt: "desc"
-      }]
+  const createDB = dbUrl => {
+   return new PouchDB(dbUrl, {
+    ajax: {
+     cache: false,
+     timeout: 60000
+    },
+    auth: {
+     username: COUCHDB_USER,
+     password: COUCHDB_PASSWORD
     }
-  });
-}
-
-
-
-
-mainReplicator.put({
-  _id: "main_to_events",
-  source: mainDbUrl,
-  target: eventsDbUrl,
-  continuous: true,
-  selector: {
-    type: "EVENT"
+   });
   }
-});
-eventsReplicator.put({
-  _id: "events_to_main",
-  source: eventsDbUrl,
-  target: mainDbUrl,
-  continuous: true
-});
 
-mainReplicator.put({
-  _id: "main_to_notifications",
-  source: mainDbUrl,
-  target: notificationsDbUrl,
-  continuous: true,
-  selector: {
-    type: "notification"
-  }
-});
-notificationsReplicator.put({
-  _id: "notifications_to_main",
-  source: notificationsDbUrl,
-  target: mainDbUrl,
-  continuous: true
-});
+  const mainReplicator = await createDB(mainReplicatorUrl);
+  const eventsReplicator = await createDB(eventsReplicatorUrl);
+  const notificationsReplicator = await createDB(notificationsReplicatorUrl);
+  const archiveReplicator = await createDB(archiveReplicatorUrl);
 
-mainReplicator.put({
-  _id: "main_to_archive",
-  source: mainDbUrl,
-  target: archiveDbUrl,
-  continuous: true,
-  selector: {
-    type: "EVENT",
-    status: "done"
+  const mainDbUrl = `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@main-db:5984/db`;
+  const eventsDbUrl = `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@events-db:5984/events`;
+  const notificationsDbUrl = `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@notifications-db:5984/notifications`;
+  const archiveDbUrl = `http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@archive-db:5984/archive`;
+
+  await fetch(`http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@auth-db:5984/_users`, {method: 'PUT'});
+
+  for (db of[mainDb,
+   eventsDb,
+   notificationsDb,
+   archiveDb,
+   sharedDb,
+   publicDb,
+   anonymousDb]) {
+   await db.createIndex({
+    index: {
+     fields: [
+      {
+       createdAt: "desc"
+      }
+     ]
+    }
+   });
   }
-});
-archiveReplicator.put({
-  _id: "archive_to_main",
-  source: archiveDbUrl,
-  target: mainDbUrl,
-  continuous: true
-});
+
+  try {
+   await mainReplicator.put({
+    _id: "main_to_events",
+    source: mainDbUrl,
+    target: eventsDbUrl,
+    continuous: true,
+    selector: {
+     type: "EVENT"
+    }
+   });
+  } catch (e) {
+   if (e.status != 409) {
+    throw e;
+   }
+  }
+
+  try {
+   await eventsReplicator.put({_id: "events_to_main", source: eventsDbUrl, target: mainDbUrl, continuous: true});
+  } catch (e) {
+   if (e.status != 409) {
+    throw e;
+   }
+  }
+
+  try {
+   await mainReplicator.put({
+    _id: "main_to_notifications",
+    source: mainDbUrl,
+    target: notificationsDbUrl,
+    continuous: true,
+    selector: {
+     type: "notification"
+    }
+   });
+  } catch (e) {
+   if (e.status != 409) {
+    throw e;
+   }
+  }
+
+  try {
+   await notificationsReplicator.put({_id: "notifications_to_main", source: notificationsDbUrl, target: mainDbUrl, continuous: true});
+  } catch (e) {
+   if (e.status != 409) {
+    throw e;
+   }
+  }
+
+  try {
+   await mainReplicator.put({
+    _id: "main_to_archive",
+    source: mainDbUrl,
+    target: archiveDbUrl,
+    continuous: true,
+    selector: {
+     type: "EVENT",
+     status: "done"
+    }
+   });
+  } catch (e) {
+   if (e.status != 409) {
+    throw e;
+   }
+  }
+
+  try {
+   await archiveReplicator.put({_id: "archive_to_main", source: archiveDbUrl, target: mainDbUrl, continuous: true});
+  } catch (e) {
+   if (e.status != 409) {
+    throw e;
+   }
+  }
+ } catch (e) {
+  console.log(e);
+  process.exit(1);
+ }
+})();
