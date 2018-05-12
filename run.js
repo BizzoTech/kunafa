045304675972455d2aba046fa1ff702590791b0d
Emad Shaaban <emad.shaaban92@gmail.com>
2018-05-12 15:03:54 +0200

    support multi env docker-compose files

--- run.js
+++ run.js
@@ -26,13 +26,13 @@ const updateYamlFile = (filePath, buildType) => {
       }
     }, serviceWithEnv);
   }, doc.services);
-  const updatedDoc = R.merge(doc, {services: updatedServices});
+  const updatedDoc = R.merge(doc, { services: updatedServices });
   fs.writeFileSync(filePath, yaml.safeDump(updatedDoc));
 }
 
 const executeCommand = command => {
   return new Promise((resolve, reject) => {
-    childProcess = exec(command, {maxBuffer: Infinity}, (err, stdout, stderr) => {
+    childProcess = exec(command, { maxBuffer: Infinity }, (err, stdout, stderr) => {
       if (err) {
         return reject(err);
       }
@@ -47,9 +47,9 @@ const executeCommand = command => {
 }
 
 
-const installPlugin = async(distDir, plugin) => {
+const installPlugin = async (distDir, plugin) => {
 
-  if(fs.existsSync(`node_modules/${plugin}/config.json`)){
+  if (fs.existsSync(`node_modules/${plugin}/config.json`)) {
     const pluginConfig = JSON.parse(await fs.readFile(`node_modules/${plugin}/config.json`));
 
     for (const fileName of pluginConfig.filesToCopy) {
@@ -66,7 +66,7 @@ const installPlugin = async(distDir, plugin) => {
 
 const dontCopy = ['dist', 'node_modules', 'package.json', 'yarn.lock', 'run.js', 'nginx-conf'];
 
-const start = async() => {
+const start = async () => {
   try {
     const currentPath = await fs.realpath('.');
     const projectName = currentPath.split('/').pop();
@@ -79,8 +79,8 @@ const start = async() => {
     // Copy kunafa content
     const kunafaDirContents = await fs.readdir('node_modules/kunafa');
     //console.log(kunafaDirContents);
-    for(fileName of kunafaDirContents){
-      if(!dontCopy.includes(fileName)){
+    for (fileName of kunafaDirContents) {
+      if (!dontCopy.includes(fileName)) {
         await fs.copy(`node_modules/kunafa/${fileName}`, `${distDir}/${fileName}`);
       }
     }
@@ -97,28 +97,32 @@ const start = async() => {
     } catch (error) {
       console.log(error);
     }
-    
-    
+
+
 
 
     // Copy app content
     const currentDirContents = await fs.readdir('.');
     //console.log(currentDirContents);
-    for(fileName of currentDirContents){
-      if(!dontCopy.includes(fileName) && !fileName.startsWith('.env')){
+    for (fileName of currentDirContents) {
+      if (!dontCopy.includes(fileName) && !fileName.startsWith('.env')) {
         await fs.copy(fileName, `${distDir}/${fileName}`);
       }
     }
 
     await fs.copy(`.env.${BUILD_TYPE}`, `${distDir}/.env`);
 
+    if (fs.existsSync(`${distDir}/docker-compose.${BUILD_TYPE}.yml`)) {
+      await fs.copy(`${distDir}/docker-compose.${BUILD_TYPE}.yml`, `${distDir}/docker-compose.override.yml`);
+    }
+
     updateYamlFile(`${distDir}/docker-compose.yml`, BUILD_TYPE);
-    if(fs.existsSync(`${distDir}/docker-compose.override.yml`)){
+    if (fs.existsSync(`${distDir}/docker-compose.override.yml`)) {
       updateYamlFile(`${distDir}/docker-compose.override.yml`, BUILD_TYPE);
     }
-    
 
-    
+
+
     const result = await executeCommand(`cd ${distDir} && docker-compose up --build --remove-orphans -d`);
     console.log(result);
   } catch (e) {
